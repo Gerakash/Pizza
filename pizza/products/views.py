213152e96cb15a542a3c33aed08b4476ed44556c
gerakash <aigerim.kash@icloud.com>
2021-02-15 18:41:38 +0600

    imports from Django

--- pizza/products/views.py
+++ pizza/products/views.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
 from .models import Product,AboutUs
-from .form import register_form
 from .form import*
 # Create your views here.
+from .filters import ProductFilter
 #jinja
+from .tokens import account_activation_token
+
+
 def homepage(request):
     products = Product.objects.all()
-    return render(request,'products/products.html',{"products":products})
+    filters = ProductFilter(request.GET,queryset=products)
+    products = filters.qs
+    return render(request,'products/products.html',{"products":products, "filters":filters})
 
 
 def aboutus_page(request):
@@ -23,32 +34,61 @@ def aboutus_page(request):
 #     return render(request,'products/contacts.html',{"contacts":contacts})
 
 def register(request):
-    form = register_form
+    form = SignupForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {"form":form}
-    return render (request,'products/register.html',context)
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            Profile.objects.create(user=user)
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('products/acc_active_email.html', {
+                'user':user,
+                'domain':current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user)
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email,]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+
+    return render(request,'products/register.html', {'form': form})
+
+
 
 def user_list(request):
-    users = User.objects.all()
+    users = Profile.objects.get(user=request.user)
     context = {'users': users}
     return render(request, 'products/user.html', context)
 
 
 def create_order(request, product_id):
+    user1 = Profile.objects.get(user=request.user)
     product = Product.objects.get(id=product_id)
     total_price = 0
-    form = OrderForm(initial={'product':product})
+    form = OrderForm(initial={'product':product,'user':request.user})
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
             total_price = product.price * form.cleaned_data['quantity']
             form.save()
+            if form.cleaned_data['payment_type'] == 'Wall':
+                if user1.wallet >= total_price:
+                    user1.wallet -= total_price
+                    return HttpResponse('Thank you for shopping!')
+                else:
+                    return HttpResponse('payment!')
+            else:
+                user1.order_count += 1
+                user1.save()
     context = {'form':form, 'total_price':total_price}
     return render(request,'products/create_order.html',context)
+
 
 def update_order(request,order_id):
     order = Order.objects.get(id=order_id)
@@ -83,6 +123,23 @@ def sign_in(request):
 def logout_page(request):
     logout(request)
     return redirect('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+
+        return HttpResponse ('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
 
 
 
